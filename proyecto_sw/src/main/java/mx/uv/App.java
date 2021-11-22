09e104a5b83291f1c2f118fc2b2f2edce5cf3aad
AridaiOrtegaZ <aridaiortega1402@gmail.com>
2021-11-22 15:50:37 -0600

    login+

--- proyecto_sw/src/main/java/mx/uv/App.java
+++ proyecto_sw/src/main/java/mx/uv/App.java
@@ -1,6 +1,9 @@
 package mx.uv;
-
+import com.google.gson.*;
 import static spark.Spark.*;
+import mx.uv.db.DAO;
+import java.util.UUID;
+import mx.uv.db.Usuario;
 
 /**
  * Hello world!
@@ -8,8 +11,10 @@ import static spark.Spark.*;
  */
 public class App 
 {
+    private static Gson gson = new Gson();
     public static void main( String[] args )
     {
+        
         options("/*", (request, response) -> {
 
             String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
@@ -26,7 +31,27 @@ public class App
         });
         
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        get("/", (req, res) -> {
+            return null;
+        });
 
+        post("/usuario", (req, res) -> {
+            // Insertamos un nuevo usuario
+            String json = req.body();
+            Usuario u = gson.fromJson(json, Usuario.class);
+            //usuarios.put(id, u);
+
+            DAO dao = new DAO();
+            JsonObject respuesta = new JsonObject();
+            respuesta.addProperty("status", dao.insertarUsuario(u));
+            return respuesta;
+        });
+        
+        get("/usuarios", (req, res) -> {
+            before((req2, res2) -> res.type("application/json"));
+            DAO dao = new DAO();
+            return gson.toJson(dao.listadoUsuario());
+        });
         
     }
 }
