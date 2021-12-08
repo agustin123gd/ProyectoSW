f9065db6ddb4547cfc26421f2b57af90a0e8122f
Yadeli Lopez <ingrid_yadeli@hotmail.com>
2021-12-08 17:06:42 -0600

    actualización

--- crearC2.js
+++ crearC2.js
@@ -1,16 +1,25 @@
+const nuevaPregunta = document.querySelector('input');
+
 function seleccionar(){
   let sel = document.getElementById('select');
   let elegido = sel.value;
 
-  document.getElementById('Seleccionado').innerText = `Ud. ha seleccionado el lenguaje ${elegido}.`;
+  document.getElementById('Seleccionado').innerText = `Ud. ha seleccionado pregunta ${elegido}.`;
+
 }
 
 function preguntaAbierta(){
-  var textInput = document.createElement('input');
-  textInput.setAttribute('type', 'button');
-  textInput.setAttribute('name', respAbierta);
+  input.textContent = 'Prueba';
+  const input = document.createElement('input');
+  nuevaPregunta.appendChild(input);
+}
+
+/*function agregarPregunta(){
+  var newDiv = document.createElement("div");
+  var newContent = document.createElement("input");
+  newDiv.appendChild(newContent);
   //document.getElementById("titulo").innerHTML = "otro intento";
 }
 document.getElementById("agregar").onclick = function(){
   preguntaAbierta();
-}
+}*/
