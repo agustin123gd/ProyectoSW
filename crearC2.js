ea4ac5231cee01a02d9a153d64f731c3de054ebe
Yadeli Lopez <ingrid_yadeli@hotmail.com>
2021-12-10 23:56:57 -0600

    axtualización

--- crearC2.js
+++ crearC2.js
@@ -30,36 +30,65 @@ function añadirPregunta(){
 
   //Primer div para titulo de pregunta y opciones de pregunta
   let div = document.createElement("div"); //crear el div
+  let pa = document.createElement("p");
   let input = document.createElement("input"); //crear el input
   let radio = document.createElement("input"); //crear radio1
   let radio2 = document.createElement("input"); //crear radio2
   //Atributos a los elementos
   div.className="contenido";
   input.id="pregunta2";
+  input.placeholder="Pregunta sin título";
+  input.style="width:400px;height:20px"
   radio.type="radio";
-  radio.value="Abierta";
+  radio.name="pregunta";
+  radio.id="RadioAbierta";
+  radio.onclick="PreguntaAbierta()";
+  radio.textContent="Abierta";
   radio2.type="radio";
-  radio2.value="OpcionMultiple";
+  radio2.name="pregunta";
+  radio2.id="RadioCerrada";
+  radio2.onclick="PreguntaCerrada()";
+  radio2.textContent="OpcionMultiple";
   //agregamos los elementos al Div
+  pa.appendChild(radio);
+  pa.appendChild(radio2);
   div.appendChild(input);
-  div.appendChild(radio);
-  div.appendChild(radio2);
+  div.appendChild(pa);
 
   //Segundo div para las preguntas opcion multiple
   let div2 = document.createElement("div");
+  let pa2 = document.createElement("p");
+  let pa3 = document.createElement("p");
+  let pa4 = document.createElement("p");
   let op1 = document.createElement("input");
   let op2 = document.createElement("input");
   let op3 = document.createElement("input");
   div2.className="preguntamulti";
-  op1.type="radio";
-  op2.type="radio";
-  op3.type="radio";
+  op1.name="resp";
+  op1.placeholder="Escribe la opcion 1";
+  op2.name="resp";
+  op2.placeholder="Escribe la opcion 2";
+  op3.name="resp";
+  op3.placeholder="Escribe la opcion 3";
 
-  div2.appendChild(op1);
-  div2.appendChild(op2);
-  div2.appendChild(op3);
+  pa2.appendChild(op1);
+  pa3.appendChild(op2);
+  pa4.appendChild(op3);
+  div.appendChild(pa2);
+  div.appendChild(pa3);
+  div.appendChild(pa4);
+
+  //tercer div para preguntas abiertas
+  let div3 = document.createElement("div");
+  let resp = document.createElement("input");
+  div3.className="preguntaabierta";
+  resp.placeholder="Escribe aquí la respuesta";
+  resp.style="width:400px;height:20px";
+
+  div3.appendChild(resp);
 
   div.appendChild(div2);
+  div.appendChild(div3);
 
   document.body.appendChild(div);
 }
