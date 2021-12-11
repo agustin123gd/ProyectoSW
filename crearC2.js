6e4ffaaa3d0b9132e20e381a49de4c266e04a231
Yadeli Lopez <ingrid_yadeli@hotmail.com>
2021-12-10 18:03:33 -0600

    actualización

--- crearC2.js
+++ crearC2.js
@@ -1,23 +1,85 @@
-const nuevaPregunta = document.querySelector('input');
+//const nuevaPregunta = document.querySelector('input');
+//var NoPreguntas = 1;
+//let CkAbierta = document.getElementById("RadioAbierta");
+//let CkCerrada = document.getElementById("RadioCerrada");
 
-const input = document.createElement('input');
-input.textContent = 'Prueba';
-nuevaPregunta.appendChild(input);
-
-function seleccionar(){
-  let sel = document.getElementById('select');
-  let elegido = sel.value;
-
-  document.getElementById('Seleccionado').innerText = `Ud. ha seleccionado pregunta ${elegido}.`;
-
+function PreguntaCerrada() {
+  div = document.getElementById('preguntaabierta');
+  div.style.display = 'none';
+  div = document.getElementById('preguntacerrada');
+  div.style.display = '';
 }
 
-/*function agregarPregunta(){
-  var newDiv = document.createElement("div");
-  var newContent = document.createElement("input");
-  newDiv.appendChild(newContent);
-  //document.getElementById("titulo").innerHTML = "otro intento";
+function Agregar(){
+    
+  var e = document.getElementById("btnAgregarMayor" + NoPreguntas);
+  var e_eliminar = document.getElementById("btnAgregar" + NoPreguntas);
+  var throwawayNode = e.removeChild(e_eliminar);
+  NoPreguntas++;
+  $('#secundario').append(actualizarplantilla);
 }
-document.getElementById("agregar").onclick = function(){
-  preguntaAbierta();
-}*/
+
+function PreguntaAbierta() {
+  div = document.getElementById('preguntacerrada');
+  div.style.display = 'none';
+  div = document.getElementById('preguntaabierta');
+  div.style.display = '';
+}
+
+function añadirPregunta(){
+  let div = document.createElement("div");
+  let input = document.createElement("input");
+  
+  document.body.appendChild(input);
+}
+
+//creamos una clase para añadir la nueva pregunta
+/*class nuevaPregunta extends HTMLElement {
+  constructor() {
+    //llamar al super constructor
+    super();
+
+    //crear un shadow root
+    var shadow = this.attachShadow({mode: 'open'});
+
+    //crear elementos
+    var info = document.createElement('span');
+    info.setAttribute('class', 'info');
+
+    //coger el contenido del atributo text y ponerlo en el span info
+    var text = this.getAttribute('text');
+    info.textContent = text;
+
+    //crear los estilos Css e incluirlos en el shadow DOM
+    var style = document.createElement('style');
+    style.textContent = '.wrapper {' +
+                              '}' +
+
+                              '.info {' +
+                              'font-size: 0.8rem;' +
+                              'width: 200px;' +
+                              'display: inline-block;' +
+                              'border: 1px solid black;' +
+                              'padding: 10px;' +
+                              'background: white;' +
+                              'border-radius: 10px;' +
+                              'opacity: 0;' +
+                              'transition: 0.6s all;' +
+                              'position: absolute;' +
+                              'bottom: 20px;' +
+                              'left: 10px;' +
+                              'z-index: 3;' +
+                            '}';
+                       
+    //adjuntar los elementos creados al shadow DOM 
+    //notese que el span wrapper contiene los elementos
+
+    shadow.appendChild(this.style);
+    shadow.appendChild(wrapper);
+    shadow.appendChild(info);
+
+  }
+}
+
+//definir nuevo elemento
+customElements.define('nuevaPregunta', nuevaPregunta); */
